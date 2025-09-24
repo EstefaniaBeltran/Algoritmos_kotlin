59958a575dbdac516cf17029c9f2320d499a5072
EstefaniaBeltran <y.estefania.bs@gmail.com>
2025-09-24 08:38:03 -0500

    Update euclides.kt

--- algoritmos.kt/euclides.kt
+++ algoritmos.kt/euclides.kt
@@ -1,1 +1,19 @@
+fun al_euclides(num1: Int, num2: Int): Int {
+    var a = num1
+    var b = num2
 
+    
+    while (b != 0) {
+        val temp = a % b  
+        a = b 
+        b = temp  
+    }
+    return a 
+}
+
+fun main() {
+    val numero1 = 57
+    val numero2 = 75
+
+    println("El MCD entre el numero $numero1 y el numero $numero2 es: ${al_euclides(numero1, numero2)}")
+}
