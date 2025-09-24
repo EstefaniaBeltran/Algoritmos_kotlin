4b1e8572947bbbf034417cf8198ce1fd432c4992
EstefaniaBeltran <y.estefania.bs@gmail.com>
2025-09-24 08:18:52 -0500

    Update primo.kt

--- algoritmos.kt/primo.kt
+++ algoritmos.kt/primo.kt
@@ -1,1 +1,40 @@
-# Algoritmos_kotlin
+
+import kotlin.math.sqrt
+
+
+
+fun numPrimo(n: Int): Boolean {
+    if (n <= 1) 
+    {
+        return false
+    }
+    if (n == 2)
+    {
+        return true 
+    }
+    if (n % 2 == 0) 
+    {
+        return false  
+    }
+    
+    for (i in 3..sqrt(n.toDouble()).toInt() step 2) {
+        if (n % i == 0) 
+        {
+            return false 
+        }
+    }
+    return true  
+}
+
+
+fun main() {
+    val numero = 7  
+    if (numPrimo(numero))
+    {
+        println("$numero es primo")
+    } 
+    else 
+    {
+        println("$numero no es primo")
+    }
+}
