4debe4b3545b0db5200622d34ff8fc50cd7ac9bf
EstefaniaBeltran <y.estefania.bs@gmail.com>
2025-09-24 08:47:47 -0500

    Update dfs.kt

--- algoritmos.kt/dfs.kt
+++ algoritmos.kt/dfs.kt
@@ -1,1 +1,34 @@
+fun dfs(graph: List<List<Int>>, node: Int, visited: BooleanArray) 
+{
+ 
+    visited[node] = true
+    println("Visitando nodo $node")
 
+    
+    for (neighbor in graph[node]) 
+    {
+        if (!visited[neighbor]) 
+        {
+            dfs(graph, neighbor, visited)  
+        }
+    }
+}
+
+fun main() 
+{
+  
+    val graph = listOf(
+        listOf(3, 4),  
+        listOf(0, 5),  
+        listOf(3),     
+        listOf(0, 2),  
+        listOf(1),     
+        listOf(2)      
+    )
+
+    
+    val visited = BooleanArray(graph.size)
+
+    
+    dfs(graph, 0, visited)
+}
