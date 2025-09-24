e122240d68a5f8443ca73c0d5d8a2dbd31753d36
EstefaniaBeltran <y.estefania.bs@gmail.com>
2025-09-24 08:30:33 -0500

    Update factorial.kt

--- algoritmos.kt/factorial.kt
+++ algoritmos.kt/factorial.kt
@@ -1,1 +1,19 @@
-
+fun factorial(numero:Int): Int 
+{
+    if (numero == 0 || numero == 1)
+    {
+        
+        return 1
+    }
+    else 
+    {
+         return numero * factorial(numero - 1)
+    }
+}
+ 
+fun main ()
+{
+    val numero = 7  
+    val result = factorial(numero) 
+    println("El factorial de $numero es $result ")
+}
